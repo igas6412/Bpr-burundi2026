12e3d50b37535bb6dbdb78b99cb18aafaf341aaa
igas6412 <igas6412@gmail.com>
2026-09-23 07:51:22 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,87 +1,35 @@
-
-/* ============================================================
-   BURUNDI PEOPLE REGISTRY
-   firebase-config.js
-   ============================================================ */
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 import {
-    initializeApp
-} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 
 import {
     getFirestore
 } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 
-
-/* ============================================================
-   FIREBASE CONFIGURATION
-============================================================ */
-
-export const firebaseConfig = {
-
-    apiKey:
-        "AIzaSyDoojaZL4mm5UPV5ItGnVNRec_kwTxxUHM",
-
-    authDomain:
-        "burundi-people-registry.firebaseapp.com",
-
-    projectId:
-        "burundi-people-registry",
-
-    storageBucket:
-        "burundi-people-registry.firebasestorage.app",
-
-    messagingSenderId:
-        "950398020642",
-
-    appId:
-        "1:950398020642:web:9f428b5eb56af7f2946e7b",
-
-    measurementId:
-        "G-EXQLH9RBF7"
+const firebaseConfig = {
+    apiKey: "AIzaSyDoojaZL4mm5UPV5ItGnVNRec_kwTxxUHM",
+    authDomain: "burundi-people-registry.firebaseapp.com",
+    projectId: "burundi-people-registry",
+    storageBucket: "burundi-people-registry.firebasestorage.app",
+    messagingSenderId: "950398020642",
+    appId: "1:950398020642:web:9f428b5eb56af7f2946e7b",
+    measurementId: "G-EXQLH9RBF7"
 };
 
 
-/* ============================================================
-   INITIALISATION FIREBASE
-============================================================ */
-
-const app =
-    initializeApp(
-        firebaseConfig
-    );
+// Initialisation Firebase
+const app = initializeApp(firebaseConfig);
 
 
-/* ============================================================
-   FIRESTORE
-============================================================ */
-
-const db =
-    getFirestore(
-        app
-    );
+// Authentication
+const auth = getAuth(app);
 
 
-/* ============================================================
-   AUTHENTICATION
-============================================================ */
-
-const auth =
-    getAuth(
-        app
-    );
+// Firestore
+const db = getFirestore(app);
 
 
-/* ============================================================
-   EXPORTS
-============================================================ */
-
-export {
-    app,
-    db,
-    auth
-};
+// Export
+export { app, auth, db };
